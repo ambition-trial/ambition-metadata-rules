64cc6e30f8a8efa3f4ac80e4cdfc888717657292
erikvw <ew2789@gmail.com>
2018-12-19 23:57:47 -0600

    STOP CM no longer required

--- ambition_metadata_rules/tests/test_predicates.py
+++ ambition_metadata_rules/tests/test_predicates.py
@@ -111,9 +111,9 @@ class TestPredicates(AmbitionTestCaseMixin, TestCase):
     def test_pkpd_site_eq_blantyre(self):
         pc = Predicates()
         self.update_randomization_list(CONTROL)
-        self.assertTrue(pc.func_require_pkpd_stopcm(self.subject_visits[0]))
+        self.assertFalse(pc.func_require_pkpd_stopcm(self.subject_visits[0]))
         self.update_randomization_list(SINGLE_DOSE)
-        self.assertTrue(pc.func_require_pkpd_stopcm(self.subject_visits[0]))
+        self.assertFalse(pc.func_require_pkpd_stopcm(self.subject_visits[0]))
 
     @override_settings(SITE_ID=get_site_id('harare'))
     def test_pkpd_site_eq_harare(self):
